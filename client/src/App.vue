2272155cc398d8ef6fa8c419be0de80cca572477
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-06-25 20:59:19 -0400

    prepositioning: Create page files and register them with router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,5 @@
 <template>
-  <div>Hi</div>
+  <router-view>
+    
+  </router-view>
 </template>
-
-<script>
-
-export default {
-  components: {
-  },
-  data() {
-    return {};
-  },
-  provide() {},
-  methods: {
-  },
-};
-</script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: sans-serif;
-}
-
-body {
-  margin: 0;
-}
-</style>
