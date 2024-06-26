9dd731569145d27788c59c09b3bd07c687597748
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-06-25 20:53:27 -0400

    initialize

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {};
+  },
+  provide() {},
+  methods: {
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ client/src/router.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Tell it the type of routes we want to support.
+export const router = createRouter({
+    // History tells the router how to handle the history 
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: null },
+        {
+            path: '/coaches/:id',
+            component: null,
+            children: [
+                { path: 'contact', component: null }
+            ]
+        },
+        { path: '/register', component: null },
+        { path: '/requests', component: null },
+        { path: '/:notFound(.*)', component: NotFound }
+    ],
+    linkActiveClass: 'active',
+    scrollBehavior(to, from, savedPosition) {
+        // Called by the vue router, whenever our page changes. It receives 3 arguments: to, from, and savedPosition.
+        // console.log('to', to);
+        // console.log('from', from,);
+        // console.log('savedPosition', savedPosition);
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return { left: 0, top: 0 }
+    }
+});
+
+/**
+ * Built in function, it is called by the vue router when we are navigating from our route to another.
+ * It receives 3 arguments, 
+ * to = , 
+ * from = , 
+ * next = we call this to confirm, or cancel the navigation action. We can deny a user, if they are not authenticated.
+ */
+router.beforeEach((to, from, next) => {
+    // next(false) // cancel our navigation
+    console.log('Can determine if we have auth from the meta, ', to.meta.needsAuth)
+    next(); // just behave normally
+});
+
+/**
+ * Example of always forcing users to team-members route. 
+ * We do need to check if we are heading there first. Otherwise we will cause an infinite loop.
+ */
+// router.beforeEach((to, from, next) => {
+//     if (to.name === 'team-members') {
+//         next();
+//     }else {
+//         next({name: 'team-members', params: {teamId: 't2'}})
+//     }
+// });
+
+
+/**
+ * Cannot deny a navigation here, because routing has already happened. But could send analytic data to the BE here.
+ */
+router.afterEach(() => {
+    console.log('global after each')
+});
