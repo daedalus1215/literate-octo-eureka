42afac382731322f4145301c408c357bb4633364
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-06-28 22:06:11 -0400

    adding a couple of base components (base card, base button, base card.

--- client/src/pages/coaches/CoachesList.vue
+++ client/src/pages/coaches/CoachesList.vue
@@ -3,15 +3,17 @@
         FILTER
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <CoachItem v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></CoachItem>
-        </ul>
-        <h3 v-else>No Coaches Found</h3>
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <CoachItem v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></CoachItem>
+            </ul>
+            <h3 v-else>No Coaches Found</h3>
+        </base-card>
     </section>
 </template>
 
