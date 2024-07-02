330439a7b7dac3a86dd1362de07cc08168d23600
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-07-01 23:15:19 -0400

    Wire up CoachForm and get it to work with dummy data.

--- client/src/store/modules/coaches/actions.js
+++ client/src/store/modules/coaches/actions.js
@@ -1,1 +1,15 @@
-export default {};
+export default {
+    registerCoach(context, data) {
+        const coachData = {
+            id: 'c3', // dummy temp hard coded.
+            firstName: data.first,
+            lastName: data.last,
+            description: data.desc,
+            hourlyRate: data.rate,
+            areas: data.areas
+        }
+
+        console.log('coachData', coachData)
+        context.commit('registerCoach', coachData);
+    }
+};
